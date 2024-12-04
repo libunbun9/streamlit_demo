110a960e152fa9dda04e83528d6e6ab35728f3d5
alicia arifin <168159018+libunbun9@users.noreply.github.com>
2024-12-04 10:09:37 +0700

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,41 @@
+import streamlit as st
+st.write("Hello, leggo learn together")
+st.title("This is the app title")
+
+st.header("This is the header")
+
+st.markdown("This is the markdown")
+
+st.subheader("This is the subheader")
+
+st.caption("This is the caption")
+
+st.code("x = 2021")
+
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+
+st.checkbox('Yes')
+st.button('Click Me')
+st.radio('Pick your gender', ['Male', 'Female'])
+st.selectbox('Pick a fruit', ['Apple', 'Banana', 'Orange'])
+st.multiselect('Choose a planet', ['Jupiter', 'Mars', 'Neptune'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+st.slider('Pick a number', 0, 50)
+
+
+
+
+st.number_input('Pick a number', 0, 10)
+st.text_input('Email address')
+st.date_input('Traveling date')
+st.time_input('School time')
+st.text_area('Description')
+st.file_uploader('Upload a photo')
+st.color_picker('Choose your favorite color')
+
+
+
+
+
+
